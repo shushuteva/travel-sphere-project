0676c31966b90b1c11518b09f3e2ab962bd27f7f
Mihaela Shushuteva <shushutevamihaela@gmail.com>
2025-01-07 13:42:55 +0200

    first commit

---
+++ src/main/java/com/travelsphere/travelsphere/TravelsphereApplication.java
@@ -0,0 +1,13 @@
+package com.travelsphere.travelsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsphereApplication.class, args);
+	}
+
+}
